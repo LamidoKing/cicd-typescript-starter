abbd32a4f7d90fae4fce134409b18e20d5c70107
Lamido Tijjani <kinglamido@gmail.com>
2025-08-02 12:01:27 +0100

    fix: restore broken test

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -22,5 +22,5 @@ test("getAPIKey should return the api key if the authorization header is in the 
 test("getAPIKey should return null if authorization header has extra spaces", () => {
   const headers = { authorization: "ApiKey  12345" };
   const apiKey = getAPIKey(headers);
-  expect(apiKey).toBe("12345");
+  expect(apiKey).toBe("");
 });
